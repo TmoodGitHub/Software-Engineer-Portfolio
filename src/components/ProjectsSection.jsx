fcf7f33657a79476b9de717391e0d7b5181f732a
Tamer Mahmoud <tamer.m.mahmoud@gmail.com>
2025-06-27 22:31:30 -0400

    Update Project statement

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -40,8 +40,8 @@ export const ProjectsSection = () => {
         </h2>
 
         <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
-          Here are some projects built from scratch to show that I know my shit.
-          :)
+          Here are some projects I created from the ground up to showcase my
+          skills and expertise.
         </p>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
